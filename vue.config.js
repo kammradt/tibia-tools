19f5c8efd43f5c3a7a707c0cb28f123c42e76518
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-07-19 22:04:53 -0300

    :arrow_up: Adds i18n

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'br',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
