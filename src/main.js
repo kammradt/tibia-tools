866288ffd085a873159e48caa8f5e7569b85ddba
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-07-19 22:18:22 -0300

    :truck: Moves i18n to plugins folder

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue';
 import Clipboard from 'v-clipboard'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import i18n from './i18n'
+import i18n from './plugins/i18n'
 
 Vue.use(ElementUI);
 Vue.use(Clipboard)
