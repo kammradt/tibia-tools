886175241ac378dff8a1cc5ec9690792ab06f394
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-07-26 00:58:22 -0300

    :wrench: :construction: Adds temporary settings page

---
+++ src/modules/home/SettingsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row align="start" justify="center">
+      <v-col cols="9" sm="8">
+        <v-col>
+          <v-card outlined>
+            <v-card-title>
+              {{$t('common.settings')}}
+            </v-card-title>
+            <v-divider/>
+            <v-card-text class="mt-3" v-text="'🚧'" />
+          </v-card>
+        </v-col>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'CoinsConverterView'
+  }
+</script>
