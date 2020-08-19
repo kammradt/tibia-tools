f3bb14cb016ef3b74af4acba67cea3aa142cc637
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-08-19 00:00:29 -0300

    :sparkles: Adds component to switch languages

---
+++ src/modules/home/LanguageSwitcher.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-speed-dial absolute direction="bottom" right class="mr-12">
+    <template v-slot:activator>
+      <v-btn icon large>
+        <v-icon v-text="'mdi-earth'"/>
+      </v-btn>
+    </template>
+    <v-btn :key="language.lang"
+           @click="setLanguageTo(language.lang)" class="title"
+           rounded
+           small
+           v-for="language in languages"
+           v-text="language.emoji"/>
+    <v-btn class="title" href="https://github.com/kammradt/tibia-tools" link rounded small target="_blank">
+      <v-icon v-text="'mdi-xml'"/>
+    </v-btn>
+  </v-speed-dial>
+</template>
+
+<script>
+export default {
+  name: "LanguageSwitcher",
+  data: () => ({
+    languages: [
+      {emoji: 'brrrr', lang: 'br'},
+      {emoji: 'ennn', lang: 'en'},
+    ]
+  }),
+  methods: {
+    setLanguageTo(language) {
+      this.$i18n.locale = language
+      this.$root.$i18n.locale = language
+    },
+  }
+}
+</script>
