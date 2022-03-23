563697b576a4b22725455ac07e16b8980ff4bb9e
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2022-03-23 19:58:42 -0300

    feat: sparkles: adds base

---
+++ src/modules/consumables-timer/views/ConsumablesTimerView.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-container>
+      <v-row align="start" justify="center">
+        <v-col lg="9">
+          <v-col>
+            <v-card outlined>
+              <v-card-title>
+                {{ $t('ct.create.title') }}
+                <v-spacer/>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-icon class="pl-2" color="grey" small v-on="on" v-text="'mdi-help-circle'"/>
+                  </template>
+                  <span v-text="$t('ct.create.tooltip')"/>
+                </v-tooltip>
+              </v-card-title>
+              <v-divider />
+              <v-card-text class="mt-3">
+                <v-textarea
+                    dense
+                    outlined
+                />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: "ConsumablesTimerView"
+}
+</script>
+
+<style scoped>
+
+</style>
